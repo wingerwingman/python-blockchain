5dc07685dd2d9e5a7b503cc183acedab9d962fd3
Justin Winger <justinwinger4@gmail.com>
2022-02-07 14:21:38 -0700

    setup tests for block validation

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -48,3 +48,45 @@ def test_mined_block_difficulty_limits_at_1():
     mined_block = Block.mine_block(last_block, 'bar')
 
     assert mined_block.difficulty == 1
+
+
+@pytest.fixture  
+def last_block(): 
+    return Block.genesis()
+
+@pytest.fixture   
+def block(last_block): 
+    return Block.mine_block(last_block, 'test_data')
+
+def test_is_valid_block(last_block, block):
+    Block.is_valid_block(last_block, block) 
+
+def test_is_valid_block_bad_last_hash(last_block, block):
+
+    block.last_hash = 'evil_last_hash'
+
+    with pytest.raises(Exception, match='The block last_hash must be correct'):
+        Block.is_valid_block(last_block, block)
+
+def test_is_valid_bad_proof_of_work(last_block, block):
+
+    block.hash = 'fff'
+
+    with pytest.raises(Exception, match='The block hash must meet the difficulty requirement'):
+        Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_jumped_difficulty(last_block, block):
+
+    jumped_difficulty = 10
+    block.difficulty = jumped_difficulty
+    block.hash = f'{"0" * jumped_difficulty}abc'
+
+    with pytest.raises(Exception, match='The block difficulty must only adjust by 1'):
+        Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_block_hash(last_block, block):
+
+    block.hash = '000000000000000000'
+
+    with pytest.raises(Exception, match='The block hash must be correct'):
+        Block.is_valid_block(last_block, block)
