e6f91d27d049db0f8359247ceebb5188782ae08c
Justin Winger <justinwinger4@gmail.com>
2022-03-07 14:29:49 -0700

    setup deserialization

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -5,7 +5,7 @@ from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.exceptions import InvalidSignature
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 
 
 class Wallet: 
@@ -20,6 +20,7 @@ class Wallet:
             default_backend()
         )
         self.public_key = self.private_key.public_key()
+        self.serialize_public_key()
 
     def sign(self, data):
         """
@@ -28,13 +29,28 @@ class Wallet:
         return self.private_key.sign(json.dumps(data).encode('utf-8'),
             ec.ECDSA(hashes.SHA256())
         )
+
+    def serialize_public_key(self):
+        """
+        Reset the public key to its serialized version. 
+        """
+        self.public_key = self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ).decode('utf-8')
+
     @staticmethod
     def verify(public_key, data, signature):
         """
         Verify a signature.
         """
+        deserialized_public_key = serialization.load_pem_public_key(
+            public_key.encode('utf-8'),
+            default_backend()
+        )
+
         try:
-            public_key.verify(
+            deserialized_public_key.verify(
                 signature,
                 json.dumps(data).encode('utf-8'),
                 ec.ECDSA(hashes.SHA256())    
